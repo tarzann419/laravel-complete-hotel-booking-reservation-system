077419d7544756bb8f0cf36c8fe1f1b50c2c45ce
dantheman <danogbo0@gmail.com>
2024-01-06 20:44:34 +0100

    recent commit

--- app/Http/Controllers/Backend/TeamController.php
+++ app/Http/Controllers/Backend/TeamController.php
@@ -5,26 +5,30 @@ namespace App\Http\Controllers\Backend;
 use App\Models\Team;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\BookArea;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
 class TeamController extends Controller
 {
-    public function AllTeam(){
+    public function AllTeam()
+    {
         $team = Team::latest()->get();
-        return view('backend.team.all_team',compact('team'));
+        return view('backend.team.all_team', compact('team'));
     } // End Method 
 
-    public function AddTeam(){
+    public function AddTeam()
+    {
         return view('backend.team.add_team');
     } // End Method 
 
-    public function StoreTeam(Request $request){
+    public function StoreTeam(Request $request)
+    {
 
         $image = $request->file('image');
-        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-        Image::make($image)->resize(550,670)->save('upload/team/'.$name_gen);
-        $save_url = 'upload/team/'.$name_gen;
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(550, 670)->save('upload/team/' . $name_gen);
+        $save_url = 'upload/team/' . $name_gen;
 
         Team::insert([
             'name' => $request->name,
@@ -40,8 +44,134 @@ class TeamController extends Controller
         );
 
         return redirect()->route('all.team')->with($notification);
-
     } // End Method
 
+    public function EditTeam($id)
+    {
 
+        $team = Team::findOrFail($id);
+        return view('backend.team.edit_team', compact('team'));
+    } // End Method 
+
+
+    public function UpdateTeam(Request $request)
+    {
+
+        $team_id = $request->id;
+
+        if ($request->file('image')) {
+
+            $image = $request->file('image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(550, 670)->save('upload/team/' . $name_gen);
+            $save_url = 'upload/team/' . $name_gen;
+
+            Team::findOrFail($team_id)->update([
+
+                'name' => $request->name,
+                'postion' => $request->postion,
+                'facebook' => $request->facebook,
+                'image' => $save_url,
+                'created_at' => Carbon::now(),
+            ]);
+
+            $notification = array(
+                'message' => 'Team Updated With Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.team')->with($notification);
+        } else {
+
+            Team::findOrFail($team_id)->update([
+
+                'name' => $request->name,
+                'postion' => $request->postion,
+                'facebook' => $request->facebook,
+                'created_at' => Carbon::now(),
+            ]);
+
+            $notification = array(
+                'message' => 'Team Updated Without Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.team')->with($notification);
+        } // End Eles 
+
+
+    } // End Method 
+
+    public function DeleteTeam($id)
+    {
+
+        $item = Team::findOrFail($id);
+        $img = $item->image;
+        unlink($img);
+
+        Team::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Team Image Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }   // End Method 
+
+
+
+    public function BookArea(){
+
+        $book = BookArea::find(1);
+        return view('backend.bookarea.book_area',compact('book'));
+    }  // End Method 
+
+
+
+    public function BookAreaUpdate(Request $request)
+    {
+
+        $book_id = $request->id;
+
+        if ($request->file('image')) {
+
+            $image = $request->file('image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(1000, 1000)->save('upload/bookarea/' . $name_gen);
+            $save_url = 'upload/bookarea/' . $name_gen;
+
+            BookArea::findOrFail($book_id)->update([
+
+                'short_title' => $request->short_title,
+                'main_title' => $request->main_title,
+                'short_desc' => $request->short_desc,
+                'link_url' => $request->link_url,
+                'image' => $save_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Book Area Updated With Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->back()->with($notification);
+        } else {
+
+            BookArea::findOrFail($book_id)->update([
+
+                'short_title' => $request->short_title,
+                'main_title' => $request->main_title,
+                'short_desc' => $request->short_desc,
+                'link_url' => $request->link_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Book Area Updated Without Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->back()->with($notification);
+        } // End Eles 
+    } // End Method 
 }
